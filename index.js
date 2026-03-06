480694d05afc16e89b280499cd894ef8a49de2a7
hbodan <hbodan@uamv.edu.ni>
2026-03-05 22:14:36 -0600

    v17

--- index.js
+++ index.js
@@ -175,9 +175,9 @@ function sendWelcome(psid) {
           title: "¿En qué está interesado?",
           subtitle: "Selecciona una categoría",
           buttons: [
-            { type: "postback", title: "Acabados Arq.", payload: VALUES.ACABADOS },
-            { type: "postback", title: "Ventanas a medida", payload: VALUES.VENTANAS },
-            { type: "postback", title: "Puertas a medida", payload: VALUES.PUERTAS }
+            { type: "postback", title: "Ventanas", payload: VALUES.ACABADOS },
+            { type: "postback", title: "Puertas", payload: VALUES.VENTANAS },
+            { type: "postback", title: "Fachadas Comerciales", payload: VALUES.PUERTAS }
           ]
         },
         {
